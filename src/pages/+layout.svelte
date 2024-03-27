847abcc0a0073d8b950aac4314fdc7aff8a3fe7f
Ivan Izobov <izobov9@gmail.com>
2024-03-27 07:20:27 +0300

    [dev] Header Widget + routing

--- src/pages/+layout.svelte
+++ src/pages/+layout.svelte
@@ -2,15 +2,26 @@
 	import Header from "$widgets/header/ui/Header.svelte";
 
 </script>
-<div class="wrapper">
+<div class="main-wrapper">
 
     <Header />
-    <slot></slot>
+    <div class="main-wrapper-content">
+        <slot></slot>
+    </div>
 </div>
 
-<style>
-    .wrapper {
+<style lang="scss">
+    .main-wrapper {
         width: 100vw;
         height: 100vh;
+        display: flex;
+        flex-direction: column;
+
+        &-content {
+            display: flex;
+            padding: 20px;
+            flex-grow: 1;
+            justify-content: center;
+        }
     }
 </style>
--- src/widgets/header/ui/Header.svelte
+++ src/widgets/header/ui/Header.svelte
@@ -1,19 +1,46 @@
 <div class="header">
-    <div class="header-block"></div>
+	<div class="header-block header-navigation">
+		<a href="/">Home</a>
+		<a href="/about">About</a>
+		<a href="/">Todo</a>
+	</div>
+	<div class="header-block header-actions">
+		<a href="/auth">Sign In</a>
+	</div>
 </div>
 
 <style lang="scss">
-    .header {
-        width: 100%;
-        height: 60px;
-        display: flex;
-        background-color: rgb(101, 101, 255);
-        padding: 5px 20px;
+	.header {
+		width: 100%;
+		height: 60px;
+		display: flex;
+		background-color: rgb(101, 101, 255);
+		padding: 5px 20px;
+		justify-content: space-between;
 
         .header-block {
-            width: 50px;
-            height: 50px;
-            background-color: rgb(255, 101, 101);
+            display: flex;
+            gap: 20px;
+            align-items: center;
+            flex: 1;
         }
-    }
+
+		.header-navigation {
+			justify-content: flex-start;
+			
+		}
+
+		.header-actions {
+			justify-content: flex-end;
+		}
+
+		a {
+			color: white;
+			text-decoration: none;
+
+			&:hover {
+				color: rgb(201, 201, 69);
+			}
+		}
+	}
 </style>
