aa487aa0e92b24d4e0c8175279e90f3fb672e899
Ivan Izobov <izobov9@gmail.com>
2024-03-27 06:53:09 +0300

    [dev] Init

---
+++ src/pages/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Header from "$widgets/header/ui/Header.svelte";
+
+</script>
+<div class="wrapper">
+
+    <Header />
+    <slot></slot>
+</div>
+
+<style>
+    .wrapper {
+        width: 100vw;
+        height: 100vh;
+    }
+</style>
---
+++ src/widgets/header/ui/Header.svelte
@@ -0,0 +1,19 @@
+<div class="header">
+    <div class="header-block"></div>
+</div>
+
+<style lang="scss">
+    .header {
+        width: 100%;
+        height: 60px;
+        display: flex;
+        background-color: rgb(101, 101, 255);
+        padding: 5px 20px;
+
+        .header-block {
+            width: 50px;
+            height: 50px;
+            background-color: rgb(255, 101, 101);
+        }
+    }
+</style>
